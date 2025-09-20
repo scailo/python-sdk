2ed7227926bac078b4d66664af88337494243067
Pratheek Adidela <apratheek@unaxiom.com>
2025-09-21 03:18:41 +0530

    Added build script.

---
+++ build.py
@@ -0,0 +1,39 @@
+import os
+import os.path
+import shutil
+
+default_lib_folder = "lib" 
+
+# Add __init__.py to all directories
+for d in os.listdir(default_lib_folder):
+    if not os.path.isfile(os.path.join(default_lib_folder, d)):
+        open(os.path.join(default_lib_folder, d, "__init__.py"), "w").close()
+
+
+# Add __init__.py to lib root
+open(os.path.join(default_lib_folder, "__init__.py"), "w").close
+print("added __init__.py")
+
+# Rename files
+files = os.listdir(default_lib_folder)
+for file in files:
+    if file.endswith(".py"):
+        t = file.removesuffix(".py")
+        l = "_".join(t.split("."))
+        l = l.replace("_connect", "_api")
+        os.rename(f"{default_lib_folder}/{file}", f"{default_lib_folder}/{l}.py")
+
+print("files have been renamed")
+
+
+# README.md can be generated
+
+
+# Copy setup.py, README.md
+files_to_copy = ["setup.py", "README.md"]
+for file in files_to_copy:
+    shutil.copyfile(file, os.path.join(default_lib_folder, file))
+
+print(" and ".join(files_to_copy) + " have been copied")
+
+os.rename(default_lib_folder, "scailo_sdk")
