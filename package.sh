aa47f8f903b8582b7afd36c1110581668f1b8abd
Pratheek Adidela <apratheek@unaxiom.com>
2025-09-22 01:13:00 +0530

    build/ and dist/ folders are deleted before generating the builds.

--- package.sh
+++ package.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-cd scailo_sdk
+rm -rf build
+rm -rf dist
+
+# cd scailo_sdk
 python setup.py sdist bdist_wheel
 
 # Upload to test.pypi.org
 twine upload --repository testpypi dist/* --verbose
 # Upload to pypi.org
 twine upload dist/* --verbose
+
+# pip install dist/scailo_sdk-0.1.2.tar.gz
