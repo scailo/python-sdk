5dd0967a846756d52336a67c679dab0b05544360
Pratheek Adidela <apratheek@unaxiom.com>
2026-01-06 14:45:37 +0530

    Updated package.sh to build using python from .venv.

--- package.sh
+++ package.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python build.py
+.venv/bin/python build.py
 
 rm -rf build
 rm -rf dist
 
 # cd scailo_sdk
-python setup.py sdist bdist_wheel
+.venv/bin/python setup.py sdist bdist_wheel
 
 # Upload to test.pypi.org
-twine upload --repository testpypi dist/* --verbose
+.venv/bin/twine upload --repository testpypi dist/* --verbose
 # Upload to pypi.org
-twine upload dist/* --verbose
+.venv/bin/twine upload dist/* --verbose
 
 # pip install dist/scailo_sdk-0.1.2.tar.gz
